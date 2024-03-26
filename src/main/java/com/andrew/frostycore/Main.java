7868cd699f11e6648f1de63f7e0ade028c140d49
Blizzards <theblizzards0@gmail.com>
2024-03-25 22:25:25 -0500

    Added all the new command class so they can be registered

--- src/main/java/com/andrew/frostycore/Main.java
+++ src/main/java/com/andrew/frostycore/Main.java
@@ -1,12 +1,20 @@
 package com.andrew.frostycore;
 
-import com.andrew.frostycore.Commands.*;
+import com.andrew.frostycore.Commands.Player.CraftCommand;
+import com.andrew.frostycore.Commands.Player.WarpCommand;
+import com.andrew.frostycore.Commands.Player.WarpsCommand;
+import com.andrew.frostycore.Commands.Server.*;
+import com.andrew.frostycore.Commands.Staff.*;
 import com.andrew.frostycore.Database.Database;
 import com.andrew.frostycore.Listeners.ConnectionListener;
 import com.andrew.frostycore.Listeners.MenuListener;
+import com.andrew.frostycore.Listeners.ScoreBoardListener;
+import com.andrew.frostycore.Managers.NameTagManager;
 import com.andrew.frostycore.Managers.PlayerManager;
+import com.andrew.frostycore.Managers.WarpManager;
 import com.andrew.frostycore.MenuSystem.PlayerMenuUtility;
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -19,6 +27,8 @@ public final class Main extends JavaPlugin {
     private static final HashMap<Player, PlayerMenuUtility> playerMenuUtilityMap = new HashMap<>();
     private Database database;
     private PlayerManager playerManager;
+    private NameTagManager nameTagManager;
+    private WarpManager warpManager;
 
     @Override
     public void onEnable() {
@@ -29,10 +39,12 @@ public final class Main extends JavaPlugin {
         try {
             database.connect();
         } catch (Exception e) {
-            e.printStackTrace();
+            System.out.println(ChatColor.RED + "ERROR: Could not connect to the database.");
         }
 
         playerManager = new PlayerManager();
+        nameTagManager = new NameTagManager(this);
+        warpManager = new WarpManager(this);
 
         registerListeners();
         registerCommands();
@@ -49,6 +61,8 @@ public final class Main extends JavaPlugin {
 
     // RETURNS PLAYER MANAGER
     public PlayerManager getPlayerManager() { return playerManager; }
+    public NameTagManager getNameTagManager() { return nameTagManager; }
+    public WarpManager getWarpManager() { return warpManager; }
 
     // RETURNS NAME TAG MANAGER
 
@@ -56,6 +70,7 @@ public final class Main extends JavaPlugin {
     private void registerListeners() {
         registerListeners(new ConnectionListener(this));
         registerListeners(new MenuListener());
+        registerListeners(new ScoreBoardListener(this));
     }
 
     // REGISTERS LISTENERS
@@ -65,14 +80,33 @@ public final class Main extends JavaPlugin {
 
     // REGISTERS COMMANDS
     private void registerCommands() {
+
+        // PLAYER COMMANDS
         new CraftCommand(this);
+        new WarpCommand(this);
+        new WarpsCommand(this);
+
+        // SERVER COMMANDS
+        new DayCommand();
+        new DeleteWarpCommand(this);
+        new DuskCommand();
         new EnderChestCommand(this);
         new GamemodeAdventure(this);
         new GamemodeCreative(this);
         new GamemodeSpectator(this);
         new GamemodeSurvival(this);
         new KillCommand(this);
+        new MorningCommand();
+        new NightCommand();
         new RankCommand(this);
+        new SetWarpCommand(this);
+
+        // STAFF COMMANDS
+        new BanCommand(); // Need todo
+        new FreezeCommand(); // Need todo
+        new InvSeeCommand(); // Need todo
+        new KickCommand(); // Need todo
+        new MuteCommand(); // Need todo
     }
 
     public static PlayerMenuUtility getPlayerMenuUtility(Player p) {
